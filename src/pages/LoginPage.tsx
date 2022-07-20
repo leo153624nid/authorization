398076549de0c5c473866c7befb639f744c16c87
leo153624nid <barjastroy@gmail.com>
2022-07-20 18:11:53 +0300

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import Login from '../components/Login/Login'
 
 function LoginPage() {
     return (
         <div>
             <h1>Login</h1>
-
+            <Login />
             <p>
                 or <NavLink to="/register">register</NavLink>
             </p>
