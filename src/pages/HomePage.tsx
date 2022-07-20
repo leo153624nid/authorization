2bc302fedb39e771d87ff6fe9d4958b23ba98440
leo153624nid <barjastroy@gmail.com>
2022-07-20 19:32:17 +0300

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,24 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 import React from 'react'
 import { Navigate } from 'react-router-dom'
+import { useAuth, useAppDispatch } from '../store/hooks/hooks'
+import { removeUser } from '../store/slices/userSlice'
 
 function HomePage() {
-    return <Navigate replace to="/login" />
+    const { isAuth, email } = useAuth()
+    const dispatch = useAppDispatch()
+    return isAuth ? (
+        <div>
+            <h1>Welcome, {email}</h1>
+
+            <button type="button" onClick={() => dispatch(removeUser())}>
+                Log out from {email}
+            </button>
+        </div>
+    ) : (
+        <Navigate replace to="/login" />
+    )
 }
 
 export default HomePage
