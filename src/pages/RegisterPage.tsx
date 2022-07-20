d4b6ed9db763082db6b61a2304575b30e8bb2ac9
leo153624nid <barjastroy@gmail.com>
2022-07-20 18:11:55 +0300

    Update RegisterPage.tsx

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import Registration from '../components/Registration/Registration'
 
 function RegisterPage() {
     return (
         <div>
             <h1>Register</h1>
-
+            <Registration />
             <p>
                 Already have an account? <NavLink to="/login">Sing in</NavLink>
             </p>
