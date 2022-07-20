a8eb0a85ce2d7983f84bdca0096247151bf6e8b1
leo153624nid <barjastroy@gmail.com>
2022-07-20 19:32:13 +0300

    Update Login.tsx

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,16 +1,29 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 import React from 'react'
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
 import { useAppDispatch } from '../../store/hooks/hooks'
 import { setUser } from '../../store/slices/userSlice'
 import Form from '../Form/Form'
 
 function Login() {
     const dispatch = useAppDispatch()
+    const navigate = useNavigate()
 
     const handleLogin = (email, pass) => {
         const auth = getAuth()
         signInWithEmailAndPassword(auth, email, pass)
-            .then(console.log)
+            .then(({ user }) => {
+                dispatch(
+                    setUser({
+                        email: user.email,
+                        id: user.uid,
+                        token: user.refreshToken,
+                    })
+                )
+                navigate('/', { replace: true })
+            })
             .catch(console.error)
     }
 
