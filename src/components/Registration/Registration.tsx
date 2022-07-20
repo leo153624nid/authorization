19ab00f3a582ef50ca64ede67f24e539f935574d
leo153624nid <barjastroy@gmail.com>
2022-07-20 18:11:51 +0300

    Create Registration.tsx

---
+++ src/components/Registration/Registration.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
+import { useAppDispatch } from '../../store/hooks/hooks'
+import { setUser } from '../../store/slices/userSlice'
+import Form from '../Form/Form'
+
+function Registration({ title, handleClick }) {
+    const dispatch = useAppDispatch()
+
+    const handleRegister = (email, pass) => {
+        const auth = getAuth()
+        createUserWithEmailAndPassword(auth, email, pass)
+            .then(console.log)
+            .catch(console.error)
+    }
+
+    return <Form title="register" handleClick={handleRegister} />
+}
+
+export default Registration
