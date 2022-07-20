5676a962504066b1457f113380093acd255df4b1
leo153624nid <barjastroy@gmail.com>
2022-07-20 18:11:50 +0300

    Update Login.tsx

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { useAppDispatch } from '../../store/hooks/hooks'
+import { setUser } from '../../store/slices/userSlice'
+import Form from '../Form/Form'
+
+function Login({ title, handleClick }) {
+    const dispatch = useAppDispatch()
+
+    const handleLogin = (email, pass) => {
+        const auth = getAuth()
+        signInWithEmailAndPassword(auth, email, pass)
+            .then(console.log)
+            .catch(console.error)
+    }
+
+    return <Form title="login" handleClick={handleLogin} />
+}
+
+export default Login
