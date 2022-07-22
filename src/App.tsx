2e96349fdd321b3b48e79de808a63fc3ce118b2c
leo153624nid <barjastroy@gmail.com>
2022-07-22 15:55:47 +0300

    update CSS

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
+import LoginPage from './pages/LoginPage/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 
 function App() {
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -2,13 +2,20 @@
 /* eslint-disable import/no-unresolved */
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import Login from '../components/Login/Login'
+import Login from '../../components/Login/Login'
+import s from './LoginPage.module.css'
 
 function LoginPage() {
     return (
-        <div className="container-login100">
+        <div className={s.loginPage}>
+            <div className="login100-pic js-tilt" data-tilt>
+                <img src="images/img-01.png" alt="IMG" />
+            </div>
+
             <h1>Login</h1>
+
             <Login />
+
             <p>
                 or <NavLink to="/register">register</NavLink>
             </p>
