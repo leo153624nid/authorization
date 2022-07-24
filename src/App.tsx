ad0068bd80d5a98f3d39af4876b8358a46670bc8
leo153624nid <barjastroy@gmail.com>
2022-07-24 18:29:48 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage/HomePage'
 import LoginPage from './pages/LoginPage/LoginPage'
+import RecoveryPage from './pages/RecoveryPage/RecoveryPage'
 import RegisterPage from './pages/RegisterPage/RegisterPage'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
+                <Route path="/recovery" element={<RecoveryPage />} />
             </Routes>
         </div>
     )
