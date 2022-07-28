b7f316afe3eacf37b33cda4ec19df0592302bf5c
leo153624nid <barjastroy@gmail.com>
2022-07-28 21:58:01 +0300

    Create validators.ts

---
+++ src/validators/validators.ts
@@ -0,0 +1,11 @@
+export const requiredField = (value: string) => {
+    if (value) return undefined
+
+    return 'Field is required'
+}
+
+export const requiredField = (value) => {
+    if (value) return undefined
+
+    return 'Field is required'
+}
