702fb156196445f72d674cba36d3186c165f9845
leo153624nid <barjastroy@gmail.com>
2022-07-29 20:00:23 +0300

    Update Recovery.tsx

--- src/components/Recovery/Recovery.tsx
+++ src/components/Recovery/Recovery.tsx
@@ -1,13 +1,40 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
 import React from 'react'
+import {
+    // confirmPasswordReset,
+    getAuth,
+    sendPasswordResetEmail,
+} from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
 import { RESET_PASSWORD } from '../../constants'
 import Form from '../Form/Form'
 
 function Recovery() {
-    const handleRecovery = () => {
-        console.log('do not work yet')
-        alert('do not work yet') /* TODO */
+    const navigate = useNavigate()
+
+    const handleRecovery = async (email: string) => {
+        const auth = getAuth()
+        // const actionCodeSettings = {
+        //     url: 'https://www.example.com/?email=user@example.com',
+        //     iOS: {
+        //         bundleId: 'com.example.ios',
+        //     },
+        //     android: {
+        //         packageName: 'com.example.android',
+        //         installApp: true,
+        //         minimumVersion: '12',
+        //     },
+        //     handleCodeInApp: true,
+        // }
+        await sendPasswordResetEmail(
+            auth,
+            email
+            // actionCodeSettings
+        )
+        // Obtain code from user.
+        // await confirmPasswordReset(auth, code)
+        navigate('/login', { replace: true })
     }
 
     return (
