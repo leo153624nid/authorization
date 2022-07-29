63f00a7c4104a1387477aa7d07a3906144429a88
leo153624nid <barjastroy@gmail.com>
2022-07-29 20:50:51 +0300

    Update Form.tsx

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react'
-import { FaAt, FaExclamation } from 'react-icons/fa'
+import { FaExclamation } from 'react-icons/fa'
 import { BsKeyFill } from 'react-icons/bs'
 import s from './Form.module.scss'
 import {
@@ -13,6 +13,8 @@ import {
     RESET_PASSWORD,
 } from '../../constants'
 import { useInput } from '../../store/hooks/hooks'
+import InputEmail from './InputEmail/InputEmail'
+import InputPass from './InputPass/InputPass'
 
 interface FormProps {
     title: string
@@ -64,79 +66,9 @@ function Form({ title, emailMemo, passMemo, handleClick }: FormProps) {
 
     return (
         <form>
-            <div className={s.wrapInput}>
-                <input
-                    className={s.inputForm}
-                    type="email"
-                    value={email.value}
-                    onChange={(e) => email.onChange(e)}
-                    onBlur={() => email.onBlur()}
-                    onFocus={() => email.onFocus()}
-                    placeholder="email"
-                />
-                <span className={s.focusInputForm} />
-                <span className={s.symbolInput}>
-                    <FaAt />
-                </span>
-                <span
-                    className={
-                        (email.isEmpty && email.isDirty) ||
-                        (email.emailError && email.isDirty)
-                            ? s.alert
-                            : s.hidden
-                    }
-                >
-                    <FaExclamation />
-                </span>
-                <span
-                    className={
-                        (email.isEmpty && email.isDirty) ||
-                        (email.emailError && email.isDirty)
-                            ? s.alertValidate
-                            : s.hidden
-                    }
-                >
-                    {email.isEmpty || email.emailError}
-                </span>
-            </div>
+            <InputEmail email={email} />
 
-            {title !== RESET_PASSWORD && (
-                <div className={s.wrapInput}>
-                    <input
-                        className={s.inputForm}
-                        type="password"
-                        value={pass.value}
-                        onChange={(e) => pass.onChange(e)}
-                        onBlur={() => pass.onBlur()}
-                        onFocus={() => pass.onFocus()}
-                        placeholder="password"
-                    />
-                    <span className={s.focusInputForm} />
-                    <span className={s.symbolInput}>
-                        <BsKeyFill />
-                    </span>
-                    <span
-                        className={
-                            (pass.isEmpty && pass.isDirty) ||
-                            (pass.passError && pass.isDirty)
-                                ? s.alert
-                                : s.hidden
-                        }
-                    >
-                        <FaExclamation />
-                    </span>
-                    <span
-                        className={
-                            (pass.isEmpty && pass.isDirty) ||
-                            (pass.passError && pass.isDirty)
-                                ? s.alertValidate
-                                : s.hidden
-                        }
-                    >
-                        {pass.isEmpty || pass.passError}
-                    </span>
-                </div>
-            )}
+            {title !== RESET_PASSWORD && <InputPass pass={pass} />}
 
             {title === CREATE_NEW_ACCOUNT && (
                 <div className={s.wrapInput}>
