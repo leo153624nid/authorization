41c9639ebff1fc3707d2e1ecfcc251b30a488b36
leo153624nid <barjastroy@gmail.com>
2022-07-29 18:31:30 +0300

    Update validators.ts

--- src/validators/validators.ts
+++ src/validators/validators.ts
@@ -1,12 +1,13 @@
-/* eslint-disable import/prefer-default-export */
-// export const requiredField = (value: string) => {
-//     if (value) return undefined
-
-//     return 'Field is required'
-// }
-
+// Valid email is required: ex@abc.xy
 export const ValidateEmail = (email: string) => {
     // eslint-disable-next-line no-useless-escape
     const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     return mailformat.test(String(email).toLocaleLowerCase())
+}
+
+// Для проверки пароля от 6 до 20 символов, которые содержат как минимум
+// одну цифровую цифру , одну заглавную и одну строчную букву
+export const ValidatePass = (pass: string) => {
+    const passformat = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/
+    return passformat.test(pass)
 }
