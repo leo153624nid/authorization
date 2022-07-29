a1bdc723862ae3349f8b74c326dcacbc1534a106
leo153624nid <barjastroy@gmail.com>
2022-07-29 15:10:19 +0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,13 @@
 export const LOGIN = 'login'
 export const CREATE_NEW_ACCOUNT = 'create new account'
 export const RESET_PASSWORD = 'reset password'
+
+export const emailValidator = {
+    isEmpty: true,
+    minLength: 3,
+}
+
+export const passValidator = {
+    isEmpty: true,
+    minLength: 3,
+}
