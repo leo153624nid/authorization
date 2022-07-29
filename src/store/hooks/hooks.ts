81fd2ee7251c6acf6f93c16b6df437e69a194237
leo153624nid <barjastroy@gmail.com>
2022-07-29 16:30:52 +0300

    Update hooks.ts

--- src/store/hooks/hooks.ts
+++ src/store/hooks/hooks.ts
@@ -1,5 +1,8 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 import { useEffect, useState } from 'react'
 import { useSelector, useDispatch, TypedUseSelectorHook } from 'react-redux'
+import { Validator } from '../../constants'
 import type { RootState, AppDispatch } from '../reduxStore'
 
 export const useAppDispatch = () => useDispatch<AppDispatch>()
@@ -15,33 +18,39 @@ export const useAuth = () => {
     }
 }
 
-export const useValidations = (value: string, validations: object) => {
+export const useValidations = (value: string, validations: Validator) => {
     const [minLengthError, setMinLengthError] = useState(false)
     const [isEmpty, setIsEmpty] = useState(true)
 
     useEffect(() => {
-        // eslint-disable-next-line no-restricted-syntax
+        // eslint-disable-next-line no-restricted-syntax, guard-for-in
         for (const validation in validations) {
             switch (validation) {
                 case 'minLength':
-                    value.length < validations[validation]
-                        ? setMinLengthError(true)
-                        : setMinLengthError(false)
+                    if (value.length < validations[validation]) {
+                        setMinLengthError(true)
+                    } else {
+                        setMinLengthError(false)
+                    }
                     break
                 case 'isEmpty':
-                    value ? setIsEmpty(false) : setIsEmpty(true)
+                    if (value) {
+                        setIsEmpty(false)
+                    } else {
+                        setIsEmpty(true)
+                    }
                     break
-
                 default:
                     break
             }
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [value])
 
     return { minLengthError, isEmpty }
 }
 
-export const useInput = (initialValue: string, validations: object) => {
+export const useInput = (initialValue: string, validations: Validator) => {
     const [value, setValue] = useState(initialValue)
     const [isDirty, setIsDirty] = useState(false)
     const valid = useValidations(value, validations)
