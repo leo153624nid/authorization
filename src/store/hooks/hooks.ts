b0ca8c3fb073171e0116abc0b78d8e7d7f95a5fd
leo153624nid <barjastroy@gmail.com>
2022-07-29 15:10:21 +0300

    Update hooks.ts

--- src/store/hooks/hooks.ts
+++ src/store/hooks/hooks.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useSelector, useDispatch, TypedUseSelectorHook } from 'react-redux'
 import type { RootState, AppDispatch } from '../reduxStore'
 
@@ -15,11 +15,36 @@ export const useAuth = () => {
     }
 }
 
-export const useInput = (initialValue: string) => {
+export const useValidations = (value: string, validations: object) => {
+    const [minLengthError, setMinLengthError] = useState(false)
+    const [isEmpty, setIsEmpty] = useState(true)
+
+    useEffect(() => {
+        // eslint-disable-next-line no-restricted-syntax
+        for (const validation in validations) {
+            switch (validation) {
+                case 'minLength':
+                    value.length < validations[validation]
+                        ? setMinLengthError(true)
+                        : setMinLengthError(false)
+                    break
+                case 'isEmpty':
+                    value ? setIsEmpty(false) : setIsEmpty(true)
+                    break
+
+                default:
+                    break
+            }
+        }
+    }, [value])
+
+    return { minLengthError, isEmpty }
+}
+
+export const useInput = (initialValue: string, validations: object) => {
     const [value, setValue] = useState(initialValue)
     const [isDirty, setIsDirty] = useState(false)
-    // eslint-disable-next-line no-console
-    console.log(`is dirty: ${isDirty}`)
+    const valid = useValidations(value, validations)
 
     const onChange = (e: { target: { value: string } }) => {
         setValue(e.target.value)
@@ -31,7 +56,9 @@ export const useInput = (initialValue: string) => {
 
     return {
         value,
+        isDirty,
         onChange,
         onBlur,
+        ...valid,
     }
 }
